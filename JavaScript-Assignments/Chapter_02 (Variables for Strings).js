f3abe6743b5bfa8cb5923d9c43299976d8f01ff4
abraheemcoder <abraheem22468@gmail.com>
2026-02-26 15:13:55 +0500

    add Assignments

---
+++ JavaScript-Assignments/Chapter_02 (Variables for Strings).js
@@ -0,0 +1,43 @@
+// // Question_01
+// // Declare a variable called username.
+// // Answer:
+// var username;
+// console.log (username);
+
+
+// // Question_02
+// // Declare a variable called myName & assign to it a string that represents your Full Name. 
+// // Answer:
+// var myName = "Abdul Raheem";
+// console.log (myName);
+
+
+// // Question_03
+// // Write script to:
+// // a) Declare a JS variable,titled message.
+// // b) Assign “Hello World” to variable message.
+// // c) Display the message in alert box. 
+// // Answer:
+// var message;
+// console.log (message);
+
+// var message = "Hello World";
+// console.log (message);
+
+// alert (message);
+
+
+// // Question_04
+// // Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// // Data: Name = Abdul Raheem
+// //       Age = 20
+// //       Course = Full Stack Web Development
+// // Answer:
+// var fullName = "Abdul Raheem";
+// var age = "20";
+// var course = "Full Stack Web Development";
+// alert (fullName);
+// alert (age);
+// alert (course);
+
+  
