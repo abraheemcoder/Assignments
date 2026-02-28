71aee957a6cedf9975b78e26d0af0a93a9d6a1bc
abraheemcoder <abraheem22468@gmail.com>
2026-03-01 00:11:21 +0500

    add folder: Chapter_04 (Question 03)

--- JavaScript-Assignments/Chapter_04 (Variables Names Legal & Illegal).js
+++ JavaScript-Assignments/Chapter_04 (Variables Names Legal & Illegal).js
@@ -5,14 +5,29 @@
 // console.log(myName, age, birthPlace);
 
 
-// Question_02
-// Declare 5 legal & 5 illegal variable names.
-// Answer:
-// 5 Legal Variables Names:
-let myName;
-let _age;
-let $price;
-let user1;
-let total_marks;
+// // Question_02
+// // Declare 5 legal & 5 illegal variable names.
+// // Answer:
+// // 5 Legal Variables Names:
+// let myName;
+// let _age;
+// let $price;
+// let user1;
+// let total_marks;
 
-// 5 Ile
+// // 5 Illegal Variable Names:
+// let 1st;       // can't start with Number
+// let my-name;   // Hyphen not allowed
+// let var;       // JS reserved Keyword 
+// let user name; // Space not allowed
+// let @price;    // Special character not allowed
+
+
+// Question_03
+// Display this in your browser 
+// a) A starting heading “Rules for naming JS variables”.
+// b) Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable 
+// c) Variables must begin with a ______, ______ or _____. For example $user, _user or user 
+// d) Variable names are case _________.
+// e) Variable names should not be JS _________.
+// Answer in Repo: Chapter_04 (Question 03)
