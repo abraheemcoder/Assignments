671bc53b34427a972f99f2346f9cb0232d436f29
abraheemcoder <abraheem22468@gmail.com>
2026-02-28 21:50:18 +0500

    Solve Chapter_03 js Question

--- JavaScript-Assignments/Chapter_03 (Variables for Numbers).js
+++ JavaScript-Assignments/Chapter_03 (Variables for Numbers).js
@@ -2,29 +2,34 @@
 // // Declare a variable called age & assign to it your age.
 // // Show your age in an alert box.
 // // Alert: I am 20 years old
+// Answer:
 // let age = 20;
 // alert("I am "+age+" years old");
 
 
-// Question_02
-// Declare & initialize a variable to keep track of how many times a visitor has visited a web page.
-// Show his/her number of visits on your web page.
-// For example: "You have visited this site N times".
-// Answer in Repo: Chapter_03 (Question 02, 03 & 04).
+// // Question_02
+// // Declare & initialize a variable to keep track of how many times a visitor has visited a web page.
+// // Show his/her number of visits on your web page.
+// // For example: "You have visited this site N times".
+// // Answer:
+// let userVisits = 4;
+// alert("You have visited this site " + userVisits + " times")
 
 
-// Question_03
-// Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
-// Message: My birth year is 2006
-//          Data type of my declared variable is number
-// Answer in Repo: Chapter_03 (Question 02, 03 & 04).
+// // Question_03
+// // Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+// // Message: My birth year is 2006
+// //          Data type of my declared variable is number
+// // Answer in Repo: Chapter_03 (Question 02, 03 & 04).
+// https://github.com/abraheemcoder/Web-Dev-Assignments/tree/e06e6281145d51e654d70a6f355d33d436d4839d/JavaScript-Assignments/Chapter_03%20(Question%2002%2C%2003%20%26%2004)
 
 
-// Question_04
-// A visitor visits an online clothing store www.xyzClothing.com .
-// Write a script to store in variables the following information: 
-// a. Visitor’s name 
-// b. Product title 
-// c. Quantity i.e. how many products a visitor wants to order 
-// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”. 
-// Answer in Repo: Chapter_03 (Question 02, 03 & 04).
+// // Question_04
+// // A visitor visits an online clothing store www.xyzClothing.com .
+// // Write a script to store in variables the following information: 
+// // a. Visitor’s name 
+// // b. Product title 
+// // c. Quantity i.e. how many products a visitor wants to order 
+// // Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”. 
+// // Answer in Repo: Chapter_03 (Question 02, 03 & 04).
+// https://github.com/abraheemcoder/Web-Dev-Assignments/tree/e06e6281145d51e654d70a6f355d33d436d4839d/JavaScript-Assignments/Chapter_03%20(Question%2002%2C%2003%20%26%2004)
